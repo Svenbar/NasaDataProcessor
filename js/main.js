b51a43212789058e2bd0a9638b3a264dacc0acda
SVEN B <101688190+Svenbar@users.noreply.github.com>
2022-04-14 23:34:30 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//Example fetch using pokemonapi.co
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+  console.log(choice)
+  const url = `https://api.nasa.gov/planetary/apod?api_key=7shuVp1Qowekpxryl1GXP5KDMB5GFkfUmcColgOi&date=${choice}`
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        document.querySelector('h2').innerText = data.title
+        document.querySelector('img').src = data.hdurl
+        document.querySelector('h3').innerText = data.explanation
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+
